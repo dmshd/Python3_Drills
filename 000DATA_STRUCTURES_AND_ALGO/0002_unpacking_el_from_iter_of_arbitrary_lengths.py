efa1cd124401b1b136d3843fa772fa8323bb2bc8
Daniel Muyshond <daniel.muyshond@imio.be>
2023-06-13 19:17:23 +0200

    Fix comment o fmore than 88 char

--- 000DATA_STRUCTURES_AND_ALGO/0002_unpacking_el_from_iter_of_arbitrary_lengths.py
+++ 000DATA_STRUCTURES_AND_ALGO/0002_unpacking_el_from_iter_of_arbitrary_lengths.py
@@ -1,8 +1,8 @@
 """
-You need to unpack N elements from an iterable, but the iterable may be longer than N elements, causing a "too many values to unpack" exception.
+You need to unpack N elements from an iterable, but the iterable may be longer than N
+elements, causing a "too many values to unpack" exception.
 
 Python "star expressions" can be used to address this problem.
-
 """
 
 record = ("Dave", "dave@example.com", "773-555-1212", "847-555-1212")
@@ -16,10 +16,9 @@ print(phone_numbers)  # ['773-555-1212', '847-555-1212']
 print(trailing)  # [10, 8, 7, 1, 9, 5, 10]
 print(current)  # 3
 
-# The star syntax can be especially useful when iterating over a sequence of tuples of varying length.
-
-
-# star syntax is especially useful when iterating over a sequence of tuples of varying length. For example, perhaps a sequence of tagged tuples:
+# star syntax is especially useful when iterating over a sequence of tuples of varying
+# length.
+# For example, perhaps a sequence of tagged tuples:
 
 records = [
     ("foo", 1, 2),
@@ -42,7 +41,8 @@ for tag, *args in records:
     elif tag == "bar":
         do_bar(*args)
 
-# Star unpacking can also be useful when combined with certain kinds of string processing operations, such as splitting.
+# Star unpacking can also be useful when combined with certain kinds of string
+# processing operations, such as splitting.
 
 line = "nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false"
 splitted_line = line.split(
@@ -54,21 +54,26 @@ print(homedir)  # /var/empty
 print(sh)  # /usr/bin/false
 
 
-# Sometimes you might want to unpack values and throw them away. You can't just specify a bare * when unpacking, but you could use a common throwaway variable name, such as _ or ign (ignored).
+# Sometimes you might want to unpack values and throw them away. You can't just specify
+# a bare * when unpacking, but you could use a common throwaway variable name, such as
+# _ or ign (ignored).
 
 record = ("ACME", 50, 123.45, (12, 18, 2012))
 name, *_, (*_, year) = record
 print(name)  # ACME
 print(year)  # 2012
 
-# There is a certain similarity between star unpacking and list-processing features of various functional languages. For example, if you have a list, you can easily split it into head and tail components like this:
+# There is a certain similarity between star unpacking and list-processing features of
+# various functional languages. For example, if you have a list, you can easily split
+# it into head and tail components like this:
 
 items = [1, 10, 7, 4, 5, 9]
 head, *tail = items
 print(head)  # 1
 print(tail)  # [10, 7, 4, 5, 9]
 
-# One could imagine writing functions that perform such splitting in order to carry out some kind of clever recursive algorithm. For example:
+# One could imagine writing functions that perform such splitting in order to carry out
+# some kind of clever recursive algorithm. For example:
 
 items = [1, 10, 7, 4, 5, 9]
 
